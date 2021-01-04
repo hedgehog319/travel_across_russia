641c2cb3b4fa16e445bf0b762738e1dfa4e4b8dc
idrisov <karim.idrisow2016@gmail.com>
2021-01-04 17:46:39 +1000

    fix smthg

---
+++ frontend/src/components/FooterComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="footer">
+    <v-footer
+        dark
+        padless
+        absolute
+    >
+      <v-card
+          width="100%"
+          flat
+          tile
+          class="indigo lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-4 white--text"
+              icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Путешествуй по России - курсовой проект
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+  data: () => ({
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
