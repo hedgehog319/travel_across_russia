e784af3191a165a92894aba29d95b12c4d6ffd2f
hedgehog319 <fign1910@mail.ru>
2021-01-04 12:15:48 +1000

    + LoginPage.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuelidate from 'vuelidate'
+
+import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(VueAxios, axios)
+Vue.use(Vuelidate)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="login">
+    <v-form @submit.prevent="">
+      <v-container class="mw-400">
+        <v-card elevation="2" class="round">
+          <v-card-title class="text-center">Вход</v-card-title>
+          <v-container>
+            <v-text-field label="Логин" :rules="rules" hide-details="auto" class="rounded-pill"/>
+            <v-text-field label="Пароль"></v-text-field>
+          </v-container>
+
+          <v-card-actions>
+            <v-container class="text-center">
+              <v-btn elevation="0" block class="rounded-pill primary">Login</v-btn>
+              <div style="margin-top: 10px">
+                <span>Нет аккаунта? </span>
+                <router-link to="/" class="text-decoration-none blue--text text--darken-2 hover">
+                  Зарегистрируйся
+                </router-link>
+              </div>
+            </v-container>
+          </v-card-actions>
+
+        </v-card>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      rules: [
+        value => !!value || 'Необходимо заполнить поле',
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
