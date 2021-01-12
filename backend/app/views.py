d51c50e1fd8d09850880c916b3a20101780eb254
Andrey <58406255+4qwerty1@users.noreply.github.com>
2021-01-13 02:03:06 +1200

    add views for FavouriteTour and Document; redo view for Hotel

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,20 +1,19 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from rest_framework.generics import RetrieveAPIView, ListCreateAPIView
-from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, ListModelMixin, DestroyModelMixin
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 
-from app.models import Tour, Country, City, Hotel, Airline, Insurance
-from app.permissions import IsAdminOrReadOnly
+from app.models import Tour, Country, City, Hotel, Airline, Insurance, Document, FavouriteTour
+from app.permissions import IsAdminOrReadOnly, IsAdminOrCreateOnly
 from app.serializers import TourSerializer, CountrySerializer, CitySerializer, HotelSerializer, AirlineSerializer, \
-    InsuranceSerializer
+    InsuranceSerializer, DocumentSerializer, FavouriteTourSerializer
 
 
-# TODO туры(get, post, patch), документы(post, patch), отели(get)
-# TODO для посика тура (страна, продолжительность)
+# TODO профиль
 
 class TestView(ListCreateAPIView):
     queryset = Tour.objects.all()
@@ -37,12 +36,32 @@ class TourView(ModelViewSet):
         elif 'country' in params:
             country = Country.objects.get(name=params['country'])
             cities = City.objects.all().filter(country=country.id)
-            cities = list(cities.values_list('id', flat=True))
+            cities = cities.values_list('id', flat=True)
             queryset = queryset.filter(city__in=cities)
         if 'count_days' in params:
             queryset = queryset.filter(count_days__lte=params['count_days'])
 
-        return queryset
+        return super().filter_queryset(queryset)
+
+
+class FavouriteTourView(CreateModelMixin, ListModelMixin, DestroyModelMixin, GenericViewSet):
+    queryset = FavouriteTour.objects.all()
+    serializer_class = FavouriteTourSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'tour'
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def filter_queryset(self, queryset):
+        queryset = queryset.filter(user=self.request.user)
+        return super().filter_queryset(queryset)
+
+
+class DocumentView(ModelViewSet):  # redo чтобы мог редактировать пользователь
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    permission_classes = [IsAdminOrCreateOnly]
 
 
 class CountryView(ModelViewSet):
@@ -63,6 +82,21 @@ class HotelView(ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['type_of_food']
+
+    def filter_queryset(self, queryset):
+        params = self.request.query_params
+        if 'city' in params:
+            city = City.objects.get(name=params['city'])
+            queryset = queryset.filter(city=city.id)
+        elif 'country' in params:
+            country = Country.objects.get(name=params['country'])
+            cities = City.objects.all().filter(country=country.id)
+            cities = cities.values_list('id', flat=True)
+            queryset = queryset.filter(city__in=cities)
+
+        return super().filter_queryset(queryset)
 
 
 class AirlineView(ModelViewSet):
