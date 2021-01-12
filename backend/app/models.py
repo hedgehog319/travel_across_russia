a8d910c9fa506f5692f7120596ceadb3361d9be8
Andrey <58406255+4qwerty1@users.noreply.github.com>
2021-01-12 23:05:08 +1200

    upgrade models

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,11 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    access_right = models.IntegerField(default=0)
+    FOOD_CHOICES = (
+        (0, 'Пользователь'),
+        (1, 'Турменеджер'),
+        (2, 'Админ'),
+    )
+    access_right = models.PositiveSmallIntegerField(choices=FOOD_CHOICES, default=0)
     document = models.ForeignKey('Document', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f'Id: {self.id}: {self.username}'
+
+
+class FavouriteTour(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    tour = models.ForeignKey('Tour', on_delete=models.CASCADE)
 
 
 class Document(models.Model):
@@ -26,7 +36,6 @@ class Tourist(models.Model):
     booked_tour = models.ForeignKey('BookedTour', on_delete=models.CASCADE)
     document = models.ForeignKey('Document', on_delete=models.PROTECT)
     email = models.EmailField()
-    adult = models.BooleanField()
 
     def __str__(self):
         return f'Id {self.id}: {self.email}'
@@ -35,21 +44,21 @@ class Tourist(models.Model):
 # Забронированные туры
 class BookedTour(models.Model):
     tour = models.ForeignKey('Tour', on_delete=models.CASCADE)
-    airline = models.ForeignKey('Airline', on_delete=models.CASCADE)
-    insurance = models.ForeignKey('Insurance', on_delete=models.CASCADE)
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'Id {self.id}: tour_id {self.tour.id}'
+        return f'Id {self.id}: {self.tour.name}'
 
 
 class Tour(models.Model):
     name = models.CharField(max_length=150)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    count_days = models.PositiveIntegerField()
     city = models.ForeignKey('City', on_delete=models.CASCADE)
     hotel = models.ForeignKey('Hotel', on_delete=models.CASCADE)
-    count_days = models.PositiveIntegerField()
+    airline = models.ForeignKey('Airline', on_delete=models.PROTECT)
+    insurance = models.ForeignKey('Insurance', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
@@ -63,23 +72,11 @@ class Airline(models.Model):
 
 
 class Insurance(models.Model):
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    end_date = models.DateField()  # дата окончания страховки
+    price_for_day = models.DecimalField(max_digits=8, decimal_places=2)
     refund = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return f'Id {self.id}: price {self.price}'
-
-
-class Country(models.Model):
-    name = models.CharField(max_length=150)
-    is_visa = models.BooleanField(default=False)
-
-    class Meta:
-        verbose_name_plural = "Countries"
-
-    def __str__(self):
-        return f'Id {self.id}: {self.name}'
+        return f'Id {self.id}: price {self.price_for_day}'
 
 
 class Hotel(models.Model):
@@ -104,11 +101,22 @@ class Hotel(models.Model):
 
 
 class City(models.Model):
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    country = models.ForeignKey('Country', on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
 
     class Meta:
         verbose_name_plural = "Cities"
+
+    def __str__(self):
+        return f'Id {self.id}: {self.name}'
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=150)
+    is_visa = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Countries"
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
