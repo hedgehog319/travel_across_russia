32e5d3c6e8530ecbdf435a135bab7e19ea0afa1c
hedgehog319 <fign1910@mail.ru>
2021-01-12 20:28:22 +1000

    * Text

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <v-container style="background-color: #fff; padding: 5px; width: 72%" class="round">
-      <span>Лучшие предложения туров</span>
+      <div class="text-center" style="font-size: 30px; font-weight: 500;">Лучшие предложения туров</div>
 
       <v-row no-gutters v-for="n in 2" :key="n" style="margin-bottom: 10px; margin-top: 10px">
         <v-col v-for="n in 3" :key="n" cols="4" sm="4" md="4" align-self="center">
@@ -25,8 +25,7 @@ export default {
       tour: {
         title: "Море не помеха!",
         id: 0,
-        start: "21.01",
-        end: "25.01",
+        days: "21",
         country: "Россия",
         city: "Владивосток",
         cost: 200000
