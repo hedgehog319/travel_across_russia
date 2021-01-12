50fde2690bf0dfc428d194ad90204054a6e1ddac
hedgehog319 <fign1910@mail.ru>
2021-01-12 17:48:33 +1000

    + NotFoundPage.vue

---
+++ frontend/src/views/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="notfound" >
+      <span>404 Page not found</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage"
+}
+</script>
+
+<style scoped>
+
+</style>
