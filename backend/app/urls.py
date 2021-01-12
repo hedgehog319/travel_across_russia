e1d6e2bbd929b0f46ae1cd594211d6f657b782f9
Andrey <58406255+4qwerty1@users.noreply.github.com>
2021-01-13 02:01:53 +1200

    add urls for FavouriteTour and Document

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from app.views import TourView, CountryView, TestView, CityView, HotelView, AirlineView, InsuranceView
+from app.views import TourView, CountryView, TestView, CityView, HotelView, AirlineView, InsuranceView, DocumentView, \
+    FavouriteTourView
 
 router = SimpleRouter()
-router.register('tours', TourView)
+router.register('tours', TourView)  # ?city=CITY_NAME & country=COUNTRY_NAME & count_days=7
+router.register('documents', DocumentView)
+router.register('fav-tours', FavouriteTourView)  # /fav-tour/TOUR_ID/ - удаление
 router.register('countries', CountryView)
 router.register('cities', CityView)  # ?country=COUNTRY_ID, или ?country_name=Россия
 router.register('hotels', HotelView)
