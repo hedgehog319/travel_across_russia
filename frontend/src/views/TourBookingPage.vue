c3f909a29d75ab63c629f58beda24f3b5e1b809a
hedgehog319 <fign1910@mail.ru>
2021-01-13 20:23:10 +1000

    * Start stepper 3

--- frontend/src/views/TourBookingPage.vue
+++ frontend/src/views/TourBookingPage.vue
@@ -18,7 +18,7 @@
 
             <v-container style="display: flex">
               <!--TODO home redirect-->
-              <v-btn @click="e1 = 1">Отмена</v-btn>
+              <v-btn @click="$router.push({name: 'home'})">Отмена</v-btn>
               <v-spacer/>
               <v-btn style="margin-right: 5px;" color="primary" @click="e1 = 2">Далее</v-btn>
             </v-container>
@@ -54,14 +54,41 @@
           </v-stepper-content>
 
           <v-stepper-content step="3">
-            <v-card class="mb-12" color="grey lighten-1" height="300px">
+            <v-card class="mb-12" color="lighten-1" height="300px" elevation="0">
+              <v-row style="margin-top: 5px" justify="center">
+                <v-col style="padding: 0 12px" md="4">
+                  <v-text-field height="35" v-model="formData.cardNumber" background-color="#f0f0f0" label="Номер карты"
+                                style="border-top-left-radius: 5px; border-top-right-radius: 5px;"/>
+                </v-col>
 
+                <v-col style="padding: 0 12px" md="4">
+                  <v-text-field height="35" v-model="formData.cardName" background-color="#f0f0f0" label="Владелец"
+                                style="border-top-left-radius: 5px; border-top-right-radius: 5px;"/>
+                </v-col>
+              </v-row>
+
+              <v-row justify="center">
+                <v-col style="padding: 12px 12px; margin-top: 4px" md="2">
+                  <v-select label="Месяц" solo dense v-model="formData.cardMonth"
+                            :items="months"/>
+                </v-col>
+
+                <v-col style="padding: 12px 12px; margin-top: 4px" md="2">
+                  <v-select label="Год" solo dense v-model="formData.cardYear"
+                            :items="years"/>
+                </v-col>
+
+                <v-col style="padding: 0 12px;" md="2">
+                  <v-text-field v-model="formData.cardCvv" background-color="#f0f0f0" label="CVV"
+                                style="border-top-left-radius: 5px; border-top-right-radius: 5px;"/>
+                </v-col>
+              </v-row>
             </v-card>
 
             <v-container style="display: flex">
               <v-btn @click="e1 = 2">Назад</v-btn>
               <v-spacer/>
-              <v-btn style="margin-right: 5px;" color="primary" @click="e1 = 1">Далее</v-btn>
+              <v-btn style="margin-right: 5px;" color="primary" @click="e1 = 1">Завершить</v-btn>
             </v-container>
           </v-stepper-content>
         </v-stepper-items>
@@ -69,14 +96,12 @@
     </v-container>
 
     <v-dialog v-model="dialog" max-width="500px">
-      <!-- persistent - не закрывать при нажатии вне dialog-->
-
       <v-card>
         <v-card-title class="justify-center"><span class="headline">Турист</span></v-card-title>
 
         <v-card-text>
           <v-container>
-            <v-row>
+            <v-row justify="center">
               <v-col cols="12" sm="6" md="4">
                 <v-text-field label="Фамилия" v-model="selectedTourist.lastname"/>
               </v-col>
@@ -150,6 +175,10 @@ export default {
   data() {
     return {
       e1: 1,
+      dialog: false,
+      birthDayMenu: false,
+      creating: false,
+      touristID: null,
       tourists: [],
       selectedTourist: {
         firstname: null,
@@ -159,10 +188,32 @@ export default {
         series: null,
         number: null
       },
-      dialog: false,
-      birthDayMenu: false,
-      creating: false,
-      touristID: null
+      formData: {
+        cardName: '',
+        cardNumber: '',
+        cardMonth: '',
+        cardYear: '',
+        cardCvv: ''
+      },
+    }
+  },
+  computed: {
+    months() {
+      const months = []
+      for (let i = 1; i <= 12; i++) {
+        months.push(i < 10 ? '0' + i : i)
+      }
+
+      return months
+    },
+    years() {
+      const years = []
+      const year = new Date().getFullYear()
+      for (let i = 0; i < 12; i++) {
+        years.push(year + i)
+      }
+
+      return years
     }
   },
   methods: {
@@ -198,8 +249,8 @@ export default {
       this.tourists[this.touristID] = {...this.selectedTourist}
 
       this.dialog = false
-    }
-  },
+    },
+  }
 }
 </script>
 
