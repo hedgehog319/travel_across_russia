a9d20f72911e543528abc688dfa6f5c11b290f95
hedgehog319 <fign1910@mail.ru>
2021-01-14 00:07:54 +1000

    + Add registration request

--- frontend/src/views/RegistrationPage.vue
+++ frontend/src/views/RegistrationPage.vue
@@ -4,20 +4,25 @@
       <v-container class="mw-400">
         <v-card class="round" elevation="2">
           <v-card-title class="text-center">Регистрация</v-card-title>
+
+          <span v-if="invalidUser" class="text-center error--text"
+                style="position: absolute; text-align: center; left: 77px; top: 50px; max-width: 214px">
+            Пользователь с таким логином уже существует</span>
+
           <v-container>
-            <v-text-field v-model="form.login" :error-messages="loginErrors"
+            <v-text-field v-model="user.username" :error-messages="usernameErrors"
                           label="Логин"
-                          @input="this.$v.form.login.$touch"
+                          @input="inputHandler('username')"
                           @keydown.space.prevent="null"/> <!--@keydown.space.prevent - перехватывает пробел-->
 
-            <v-text-field v-model="form.email" :error-messages="emailErrors"
+            <v-text-field v-model="user.email" :error-messages="emailErrors"
                           label="Email"
                           @keydown.space.prevent="null"/> <!--@keydown.space.prevent - перехватывает пробел-->
 
-            <v-text-field v-model="form.password" :error-messages="passwordErrors"
+            <v-text-field v-model="user.password" :error-messages="passwordErrors"
                           :type="showPassword ? 'text' : 'password'"
                           label="Пароль"
-                          @input="this.$v.form.password.$touch"
+                          @input="this.$v.user.password.$touch"
                           @keydown.space.prevent="null"/>
             <v-btn icon style="position: absolute; left: auto; right: 10px; top: auto; bottom: 144px"
                    @click="showPassword = !showPassword">
@@ -26,7 +31,8 @@
           </v-container>
           <v-card-actions>
             <v-container class="text-center">
-              <v-btn block class="rounded-pill primary" elevation="0" type="submit">Загрегистрироваться</v-btn>
+              <v-btn block class="rounded-pill primary" elevation="0" type="submit">Зарегистрироваться</v-btn>
+
               <div style="margin-top: 10px">
                 <span>Уже зарегистрировались? </span>
                 <router-link class="text-decoration-none blue--text text--darken-2 hover" to="/login">
@@ -38,6 +44,8 @@
         </v-card>
       </v-container>
     </v-form>
+
+    <v-snackbar v-model="saved">Пользователь {{ user.username }} успешно создан</v-snackbar>
   </div>
 </template>
 
@@ -48,17 +56,19 @@ export default {
   name: "RegistrationPage",
   data() {
     return {
-      form: {
-        login: null,
+      user: {
+        username: null,
         email: null,
         password: null
       },
-      showPassword: false
+      showPassword: false,
+      invalidUser: false,
+      saved: false
     }
   },
   validations: {
-    form: {
-      login: {
+    user: {
+      username: {
         required,
         ruLetter: (value) => !(/[а-я]/.test(value) || /[А-Я]/.test(value)),
       },
@@ -73,34 +83,41 @@ export default {
     }
   },
   computed: {
-    loginErrors() {
+    usernameErrors() {
       let mess = ''
-      if (!this.$v.form.login.$dirty) return mess
-      if (!this.$v.form.login.required) mess = 'Введите логин'
-      else if (!this.$v.form.login.ruLetter) mess = 'Логин не должен содержать русскх букв'
+      if (!this.$v.user.username.$dirty) return mess
+      if (!this.$v.user.username.required) mess = 'Введите логин'
+      else if (!this.$v.user.username.ruLetter) mess = 'Логин не должен содержать русскх букв'
 
       return mess
     },
     emailErrors() {
       let mess = ''
-      if (!this.$v.form.email.$dirty) return mess
-      if (!this.$v.form.email.required) mess = 'Введите email'
-      else if (!this.$v.form.email.email) mess = 'Введён неверный email'
+      if (!this.$v.user.email.$dirty) return mess
+      if (!this.$v.user.email.required) mess = 'Введите email'
+      else if (!this.$v.user.email.email) mess = 'Введён неверный email'
 
       return mess
     },
     passwordErrors() {
       let mess = ''
-      if (!this.$v.form.password.$dirty) return mess
-      if (!this.$v.form.password.required) mess = 'Введите пароль'
-      else if (!this.$v.form.password.ruLetter) mess = 'Пароль не должен содержать русскх букв'
+      if (!this.$v.user.password.$dirty) return mess
+      if (!this.$v.user.password.required) mess = 'Введите пароль'
+      else if (!this.$v.user.password.ruLetter) mess = 'Пароль не должен содержать русскх букв'
 
       return mess
     }
   },
   methods: {
-    sendUser() {
-      // TODO Написать запрос
+    async sendUser() {
+      const http = this.axios.create({baseURL: 'http://127.0.0.1:8000/'});
+      await http.post(`/auth/users/`, this.user).then(res => {
+        console.log(res.statusText)
+        if (res.statusText === "Created") {
+          this.saved = true;
+        }
+      })
+          .catch(() => this.invalidUser = true)
     },
     validateUser() {
       this.$v.$touch()
@@ -108,6 +125,10 @@ export default {
       if (!this.$v.$invalid) {
         this.sendUser()
       }
+    },
+    inputHandler(field) {
+      this.invalidUser = false
+      this.$v.user[field].$touch()
     },
   }
 }
