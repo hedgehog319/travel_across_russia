b6823e53ddecd2aa54cfb40140c8efc1095d1559
hedgehog319 <fign1910@mail.ru>
2021-01-15 15:29:54 +1000

    * Some changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('', include('app.urls')),
+    path('api/', include('app.urls')),
 ]
 
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
