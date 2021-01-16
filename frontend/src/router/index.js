d17514e751a9d6fdc185ad790ae76b499b1e820e
hedgehog319 <fign1910@mail.ru>
2021-01-16 16:50:14 +1000

    * Some changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
         name: 'home',
         component: HomePage,
         meta: {
-            title: 'Путешествуй!'
+            title: 'Путешествуй по России!'
         }
     },
     {
@@ -106,5 +106,10 @@ const router = new VueRouter({
     base: process.env.BASE_URL,
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title
+    next()
+});
 
 export default router
