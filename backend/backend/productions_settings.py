793b7a54e615f7e462da137d671241f62b03dbee
Andrey <58406255+4qwerty1@users.noreply.github.com>
2021-01-31 23:52:01 +1200

    fix productions_settings.py

--- backend/backend/productions_settings.py
+++ backend/backend/productions_settings.py
@@ -3,20 +3,20 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('HOSTS')]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'course',  # data base name
-        'USER': 'course',
-        'PASSWORD': 'qwerty',
+        'NAME': os.getenv('DB_NAME'),  # data base name
+        'USER': os.getenv('DB_USER_NAME'),
+        'PASSWORD': os.getenv('DB_USER_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
