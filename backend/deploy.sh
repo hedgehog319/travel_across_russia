e968f0af39317a5a7a4969fed086c845f7a2f80b
hedgehog319 <fign1910@mail.ru>
2021-01-31 19:09:08 +1000

    * To deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -5,6 +5,6 @@ python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 
 echo "done"
