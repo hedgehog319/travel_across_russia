826972fb70df5bd00a389e458dff44c128bab5ef
Andrey <58406255+4qwerty1@users.noreply.github.com>
2021-01-31 21:09:13 +1200

    some changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,11 +45,12 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
+    "http://127.0.0.1:8080"
 ]
+
+CORS_URLS_REGEX = r'^/(api|auth)/.*$'
 
 ROOT_URLCONF = 'backend.urls'
 
