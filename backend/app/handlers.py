7cce080fdcc776d43f65258a387390a17763981a
hedgehog319 <38129787+hedgehog319@users.noreply.github.com>
2021-02-01 20:23:27 +1000

    Delete backend directory

--- backend/app/handlers.py
+++
@@ -1,8 +0,0 @@
-from corsheaders.signals import check_request_enabled
-
-
-def cors_allow_admin_to_everyone(sender, request, **kwargs):
-    return request.path.startswith('/admin/')
-
-
-check_request_enabled.connect(cors_allow_admin_to_everyone)
